bb9fb574a1cbad0a952b1a9e12188561000c2d3f
Ryan Conn <rconn478@gmail.com>
2022-06-12 13:24:09 -0500

    Use custom redux selector and dispatch

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,4 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  TypedUseSelectorHook,
+  useDispatch as useDispatchBase,
+  useSelector as useSelectorBase
+} from "react-redux";
 import { cameraSettingsReducer } from "./cameraSettings";
 
 const store = configureStore({
@@ -8,3 +13,11 @@ const store = configureStore({
 });
 
 export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+// Custom hooks to reduce overhead
+export const useDispatch = () => useDispatchBase<AppDispatch>()
+export const useSelector: TypedUseSelectorHook<RootState> = useSelectorBase
