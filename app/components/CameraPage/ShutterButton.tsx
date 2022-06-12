a5c158ff4ffd34f59a5a8a7be08d0a74a56110f4
Ryan Conn <rconn478@gmail.com>
2022-06-12 13:23:21 -0500

    Create placeholder shutter button to demo native integration

--- app/components/CameraPage/ShutterButton.tsx
+++ app/components/CameraPage/ShutterButton.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Alert, TouchableHighlight, View } from 'react-native';
+import { TouchableHighlight, View } from 'react-native';
+import { setAutoFocus } from '../../redux/cameraSettings';
+import { useDispatch, useSelector } from '../../redux/store';
 import styles from './styles';
 
 const ShutterButton: React.FC = () => {
+  const dispatch = useDispatch();
+  const autoFocus = useSelector((state) => state.cameraSettings.autoFocus);
+
+  const handlePress = () => {
+    dispatch(setAutoFocus(!autoFocus));
+  };
+
   return (
-    <TouchableHighlight style={styles.shutterButtonOuter} onPress={() => Alert.alert('Notice', 'Camera pressed! Great job!')}>
+    <TouchableHighlight style={styles.shutterButtonOuter} onPress={handlePress}>
       <View style={styles.shutterButtonInner} />
     </TouchableHighlight>
   );
