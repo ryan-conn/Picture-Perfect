1a49b6605171e06ee4b09cf50af524a5b9875d63
Ryan Conn <rconn478@gmail.com>
2022-06-14 09:12:25 -0500

    Improve slider - Set value directly on touch events - Run onChange when modified

--- app/components/common/Slider/Slider.tsx
+++ app/components/common/Slider/Slider.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated, LayoutChangeEvent, LayoutRectangle, PanResponder, View } from 'react-native';
+import { Animated, GestureResponderEvent, LayoutChangeEvent, LayoutRectangle, PanResponder, View } from 'react-native';
 import { ProgressBar } from 'react-native-paper';
 import { clamp } from '../../../utils/math';
 import styles from './styles';
@@ -26,30 +26,40 @@ const Slider: React.FC<SliderProps> = ({
   style={}
 }) => {
   const [lower, upper] = range;
-  const xPosition = React.useRef(new Animated.Value(0)).current;
-  const sliderRef = React.useRef<View>();
+  const value = React.useRef(new Animated.Value(valueProp)).current;
+  const sliderRef = React.useRef<View>(null);
   const [sliderElementInfo, setSliderElementInfo] = React.useState<SliderElementInfo>();
   const [pressed, setPressed] = React.useState(false);
 
   // Respond to touch events
-  const panResponder = React.useRef(
-    PanResponder.create({
+  const panResponder = React.useMemo(() => {
+    const layout: SliderElementInfo = sliderElementInfo || { x: 0, y: 0, width: 20, height: 150 };
+    const updateValueFromEvent = (e: GestureResponderEvent) => {
+        // Transform value to range [0, 1] depending on position of slider
+        let newValue = clamp((e.nativeEvent.pageY - layout.y) / layout.height, 0, 1);
+        // Transform to actual range [lower, upper]
+        newValue = lower + (newValue * (upper - lower));
+        value.setValue(newValue);
+
+        handleChange(newValue);
+    };
+    return PanResponder.create({
       onPanResponderGrant: (e) => {
         setPressed(true);
-        xPosition.setValue(e.nativeEvent.pageY);
+        updateValueFromEvent(e);
       },
       onPanResponderRelease: () => setPressed(false),
       onPanResponderMove: (e) => {
-        xPosition.setValue(e.nativeEvent.pageY);
+        updateValueFromEvent(e);
       }
-    })
-  ).current;
+    });
+  }, [sliderElementInfo]);
 
   // Create slider knob that gives feedback when pressed and moves with touch
   let slider = undefined;
   if (sliderElementInfo) {
-    const sliderFilledWidth = xPosition.interpolate({
-        inputRange: [sliderElementInfo.y, sliderElementInfo.y + sliderElementInfo.height],
+    const sliderFilledWidth = value.interpolate({
+        inputRange: [lower, upper],
         outputRange: [0, sliderElementInfo.height],
         extrapolate: "clamp",
     });
@@ -74,9 +84,6 @@ const Slider: React.FC<SliderProps> = ({
 
   const handleLayout = (e: LayoutChangeEvent) => {
     sliderRef.current?.measure((_x, _y, width, height, pageX, pageY) => {
-      // Determine position to place slider based on value prop
-      xPosition.setValue((valueProp - lower) / (upper - lower) * height + pageY);
-
       // Rerender component now that slider knob location can be calculated
       setSliderElementInfo({
         x: pageX,
